e6e3667f7b2a117fa95f7a8538434d3f554cdf61
Jejen Jaenudin <1125170031@global.ac.id>
2025-10-17 15:42:27 +0700

    membuat random

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math'; //untuk fungsi random
 
 // Mulai dari membuat aplikasi Flutter
 void main() {
@@ -28,34 +29,38 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title; //inial title (static saat kontruktor dipanggil)
+  
   @override
   State<HomePage> createState() => _HomePageState();
 }
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
-  String _currentTitle = ''; //state title yang akan berubah
-  int _newsIndex = 0; // index berita yang sedang tampil
+  String _currentNews = ''; //state title yang akan berubah
+  final Random _random = Random(); //objek random
 
   final List<String> dataNews = [
     "Berita tentang sepak bola",
     "Berita tentang open trip mancing",
     "Berita tentang teknologi AI",
     "Berita tentang hacker",
+    "Berita timnas gagal masuk ke round5",
+    "Berita Jobfair di selenggarakan di tangerang"
+    "Berita Belajar flutter"
   ];
 
   @override
   void initState(){
   super.initState();
-   _currentTitle = dataNews[_newsIndex]; // tampilkan berita pertama saat mulai
+   _currentNews = dataNews[_random.nextInt(dataNews.length)];// tampilkan berita pertama saat mulai
   }
 
 void _incrementCounter() {
   setState((){
   _counter++;
   //ubah title setiap kali tombol ditekan
-  _newsIndex = (_newsIndex + 1) % dataNews.length;
+  int randomIndex = _random.nextInt(dataNews.length);
     // Ubah title sesuai berita
- _currentTitle = dataNews[_newsIndex];
+ _currentNews = dataNews[randomIndex];
   });
   }
 
@@ -65,7 +70,7 @@ Widget build (BuildContext context){
   return Scaffold(
   appBar:AppBar(
     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    title: Text(_currentTitle), //gunakan title dinamis
+     title: Text(widget.title), //gunakan title dinamis
     ),
     body:Center(
       child: Column(
@@ -76,6 +81,20 @@ Widget build (BuildContext context){
             '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
               ),
+              const SizedBox(height: 30),
+            const Text(
+              'Berita Terbaru:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+               Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _currentNews,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16, color: Colors.black87),
+              ),
+            ),
+       
             ],
           ),
         ),
